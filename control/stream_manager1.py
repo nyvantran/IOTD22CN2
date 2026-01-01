7a54a293de2988d79eed0b40307d9d81fe923683
nyvantran <tondaithanh0@gmail.com>
2026-01-01 13:02:06 +0700

    1

--- control/stream_manager1.py
+++ control/stream_manager1.py
@@ -5,6 +5,8 @@ import threading
 from collections import defaultdict
 import time
 
+from control.stream_manager import stream_manager
+
 
 class StreamManager:
     """
@@ -134,7 +136,7 @@ class StreamManager:
 
 
 def main():
-    esp_ip = "192.168.1.27"
+    esp_ip = "10.251.0.62"
     udp_port = 8888
     stream_manager = StreamManager(esp_ip, udp_port)
     stream_manager.start()
@@ -156,3 +158,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+stream_manager = StreamManager(esp_ip="10.251.0.62", port=8888)
+stream_manager.start()
